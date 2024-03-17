950a1f84da339ecefcab7a95cd2cf2499e8c6805
eskokado <eskokado@gmail.com>
2024-03-17 16:55:26 -0300

    fix(users): add news fields for devise token

--- db/schema.rb
+++ db/schema.rb
@@ -98,11 +98,14 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_25_145408) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
+    t.string "provider", default: "email", null: false
+    t.string "uid", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "email", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.json "tokens"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
