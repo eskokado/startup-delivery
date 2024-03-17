6d1e4a7d7e46dca76cf9fcb9a8f5b1143fdb309d
eskokado <eskokado@gmail.com>
2024-03-17 11:18:35 -0300

    test(done_controller): add login user and fix i18n

--- spec/controllers/manager/goals/done_controller_spec.rb
+++ spec/controllers/manager/goals/done_controller_spec.rb
@@ -5,13 +5,18 @@ RSpec.describe Manager::Goals::DoneController,
   let(:goal) { create(:goal) }
   let(:goals) { create_list(:goal, 3) }
 
+  before do
+    user = FactoryBot.create(:user) # Crie um usuário válido para autenticar
+    sign_in user # Faça o login do usuário criado
+  end
+
   context 'POST #index' do
     it 'status todo to done' do
       expect do
         post :index,
              params: { goal_id: goal.id }
       end.to change { goal.reload.status }.from('todo').to('done')
-      expect(flash[:notice]).to eq(I18n.t('controllers.manager.goals.done'))
+      expect(flash[:notice]).to eq(I18n.t('controllers.manager.goals.done.one'))
     end
   end
 
@@ -21,7 +26,7 @@ RSpec.describe Manager::Goals::DoneController,
         post :show,
              params: { goal_id: goal.id }
       end.to change { goal.reload.status }.from('todo').to('done')
-      expect(flash[:notice]).to eq(I18n.t('controllers.manager.goals.done'))
+      expect(flash[:notice]).to eq(I18n.t('controllers.manager.goals.done.one'))
     end
   end
 
@@ -32,7 +37,7 @@ RSpec.describe Manager::Goals::DoneController,
              params: { goal_ids: goals.pluck(:id) }, as: :json
       end.to change { goals.first.reload.status }.from('todo').to('done')
       expect(JSON.parse(response.body)['message'])
-        .to(eq(I18n.t('controllers.manager.goals.dones')))
+        .to(eq(I18n.t('controllers.manager.goals.done.other')))
     end
   end
 end
