c76f85aad43c7a38c2f71737a5a1179378a871a0
eskokado <eskokado@gmail.com>
2024-03-17 09:15:31 -0300

    config(rails_helper): add devise test in controller

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -40,6 +40,7 @@ rescue ActiveRecord::PendingMigrationError => e
 end
 RSpec.configure do |config|
   config.include Devise::Test::IntegrationHelpers, type: :feature
+  config.include Devise::Test::ControllerHelpers, type: :controller
 
   config.include ApplicationHelper
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
