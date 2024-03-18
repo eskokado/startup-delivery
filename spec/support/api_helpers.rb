0a14c18d26f4766c902df80d890e5de281569f52
eskokado <eskokado@gmail.com>
2024-03-18 16:21:46 -0300

    feat(api_helpers): create helpers

---
+++ spec/support/api_helpers.rb
@@ -0,0 +1,19 @@
+module ApiHelpers
+  def auth_user(user)
+    post '/auth/sign_in', params: { email: user.email, password: user.password }.to_json, headers: { 'Content-Type' => 'application/json', 'Accept' => 'application/json' }
+
+    token = response.headers['access-token']
+    client = response.headers['client']
+    uid = response.headers['uid']
+
+    {
+      'access-token' => token,
+      'client' => client,
+      'uid' => uid
+    }
+  end
+
+  def json_response
+    JSON.parse(response.body)
+  end
+end
