5f2e01efb19fd002b162497fdd3ea8a14d6d677b
eskokado <eskokado@gmail.com>
2024-03-21 06:36:47 -0300

    feat(client): add soft_delete, associations with destroy soft_delete and validates

--- app/models/client.rb
+++ app/models/client.rb
@@ -3,6 +3,7 @@
 # Table name: clients
 #
 #  id         :bigint           not null, primary key
+#  deleted_at :datetime
 #  document   :string
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
@@ -10,8 +11,19 @@
 #
 # Indexes
 #
-#  index_clients_on_user_id  (user_id)
+#  index_clients_on_deleted_at  (deleted_at)
+#  index_clients_on_user_id     (user_id)
 #
 class Client < ApplicationRecord
+  acts_as_paranoid
+
   belongs_to :user
+  has_many :goals, dependent: :destroy
+
+  validates :document, presence: true
+
+  def destroy
+    goals.update_all(deleted_at: Time.current) if persisted?
+    super
+  end
 end
