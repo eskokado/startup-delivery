055a78a54d51079d3000e5028c6dcf02c1c38ba0
eskokado <eskokado@gmail.com>
2024-03-21 06:41:13 -0300

    test(client): Associations

--- spec/models/client_spec.rb
+++ spec/models/client_spec.rb
@@ -3,6 +3,7 @@
 # Table name: clients
 #
 #  id         :bigint           not null, primary key
+#  deleted_at :datetime
 #  document   :string
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
@@ -10,10 +11,14 @@
 #
 # Indexes
 #
-#  index_clients_on_user_id  (user_id)
+#  index_clients_on_deleted_at  (deleted_at)
+#  index_clients_on_user_id     (user_id)
 #
 require 'rails_helper'
 
 RSpec.describe Client, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Associations" do
+    it { should belong_to(:user) }
+    it { should have_many(:goals).dependent(:destroy) }
+  end
 end
