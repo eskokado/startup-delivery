fad4f384946078ebf9f7d321d5587f8d9b875608
eskokado <eskokado@gmail.com>
2024-03-23 14:15:42 -0300

    fix(done_controller): name methods and add turbo stream

--- app/controllers/manager/goals/done_controller.rb
+++ app/controllers/manager/goals/done_controller.rb
@@ -1,44 +1,31 @@
 module Manager
   module Goals
     class DoneController < InternalController
-      before_action :get_goal,
-                    only: %i[index show]
+      before_action :set_goal,
+                    only: %i[one]
 
-      def index
+      def one
         @goal.done!
         respond_to do |format|
-          format.html do
-            redirect_to manager_goals_path,
-                        notice: t('controllers.manager.goals.done.one')
-          end
+          format.html { redirect_to manager_goals_path, notice: t('controllers.manager.goals.done.one') }
         end
       end
 
-      def show
-        @goal.done!
-        respond_to do |format|
-          format.html do
-            redirect_to manager_goal_path(@goal),
-                        notice: t('controllers.manager.goals.done.one')
-          end
-        end
-      end
 
       def many
         Goal.where(id: params[:done][:goal_ids])
-            .update(status: :done)
+            .update_all(status: :done)
 
         respond_to do |format|
-          format.json do
-            render json: { message: t('controllers.manager.goals.done.other') },
-                   status: :ok
+          format.turbo_stream do
+            render turbo_stream: turbo_stream.replace("goals_list", partial: "manager/goals/goals/goal", collection: Goal.all)
           end
         end
       end
 
       private
 
-      def get_goal
+      def set_goal
         @goal = Goal.find(params[:goal_id])
       end
     end
