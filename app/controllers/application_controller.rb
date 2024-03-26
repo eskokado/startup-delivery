18e759f1d879880f7eb0fd4b795049d9fe036338
eskokado <eskokado@gmail.com>
2024-03-26 10:29:01 -0300

    build(application_controller): Layout/LineLength: Line is too long

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,11 +14,25 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :avatar, { client_attributes: [:document] }])
+    devise_parameter_sanitizer.permit(
+      :sign_up,
+      keys: [
+        :name,
+        :avatar,
+        { client_attributes: [:document] }
+      ]
+    )
   end
 
   def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :avatar, client_attributes: [:document]])
+    devise_parameter_sanitizer.permit(
+      :account_update,
+      keys: [
+        :name,
+        :avatar,
+        { client_attributes: [:document] }
+      ]
+    )
   end
 end
 
