e35a8b2056118085dbda110bbfe61eaca2f9fb2a
eskokado <eskokado@gmail.com>
2024-03-26 09:31:10 -0300

    feat(categories): schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_03_21_091327) do
+ActiveRecord::Schema[7.0].define(version: 2024_03_26_103114) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -68,6 +68,18 @@ ActiveRecord::Schema[7.0].define(version: 2024_03_21_091327) do
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
   end
 
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.string "image_url"
+    t.bigint "client_id", null: false
+    t.datetime "deleted_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["client_id"], name: "index_categories_on_client_id"
+    t.index ["deleted_at"], name: "index_categories_on_deleted_at"
+  end
+
   create_table "clients", force: :cascade do |t|
     t.string "document"
     t.bigint "user_id"
@@ -121,4 +133,5 @@ ActiveRecord::Schema[7.0].define(version: 2024_03_21_091327) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "categories", "clients"
 end
