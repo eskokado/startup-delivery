de1f6065e3c2ff81aba248405afd1582b733c9ce
eskokado <eskokado@gmail.com>
2024-03-26 11:48:38 -0300

    test(goals): fetch service when results are ActiveRecord::Relation orders by created_at DESC and paginates the results

---
+++ spec/businesses/goals/fetch_service_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe Goals::FetchService do
+  describe '#call' do
+    let(:date_1) { 1.day.ago.midnight }
+    let(:date_2) { 2.days.ago.midnight }
+    let(:date_3) { 3.days.ago.midnight }
+    let(:date_4) { 4.days.ago.midnight }
+    let(:date_5) { 5.days.ago.midnight }
+
+    let!(:goal_1) { create(:goal, created_at: date_1) }
+    let!(:goal_2) { create(:goal, created_at: date_2) }
+    let!(:goal_3) { create(:goal, created_at: date_3) }
+    let!(:goal_4) { create(:goal, created_at: date_4) }
+    let!(:goal_5) { create(:goal, created_at: date_5) }
+
+    context 'when results are ActiveRecord::Relation' do
+      it 'orders by created_at DESC and paginates the results' do
+        params = { page: 1 }
+        service = Goals::FetchService.new(params)
+        expect(service.call.to_a).to eq([goal_1, goal_2, goal_3, goal_4])
+      end
+    end
+  end
+end
