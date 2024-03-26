914688f5c975e99c3b9f69d66125afa270b1ea0d
eskokado <eskokado@gmail.com>
2024-03-26 09:37:12 -0300

    test(categories): validations

---
+++ spec/models/category_spec.rb
@@ -0,0 +1,31 @@
+# == Schema Information
+#
+# Table name: categories
+#
+#  id          :bigint           not null, primary key
+#  deleted_at  :datetime
+#  description :text
+#  image_url   :string
+#  name        :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  client_id   :bigint           not null
+#
+# Indexes
+#
+#  index_categories_on_client_id   (client_id)
+#  index_categories_on_deleted_at  (deleted_at)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (client_id => clients.id)
+#
+require 'rails_helper'
+
+RSpec.describe Category, type: :model do
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:description) }
+    it { should validate_presence_of(:image_url) }
+  end
+end
