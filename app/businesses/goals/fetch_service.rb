eb61b1308768d1669c4193d419f2d51639d0a548
eskokado <eskokado@gmail.com>
2024-03-26 14:08:14 -0300

    refactor(goals): fetch service

--- app/businesses/goals/fetch_service.rb
+++ app/businesses/goals/fetch_service.rb
@@ -1,18 +1,26 @@
 module Goals
   class FetchService < BusinessApplication
+    PER_PAGE = 4
+
+    attr_reader :search
     def initialize(params)
       @params = params
+      @search = Goal.ransack(@params[:q])
     end
 
     def call
-      query = Goal.ransack(@params[:q])
-      results = query.result(distinct: true)
+      results = @search.result(distinct: true)
+      paginate(results)
+    end
 
+    private
+
+    def paginate(results)
       if results.is_a?(ActiveRecord::Relation)
-        results.order('created_at DESC').page(@params[:page]).per(4)
-      else
+        results.order(created_at: :desc).page(@params[:page]).per(PER_PAGE)
+      else # Quando results é um Array
         sorted_goals = results.sort_by(&:created_at).reverse
-        Kaminari.paginate_array(sorted_goals).page(@params[:page]).per(4)
+        Kaminari.paginate_array(sorted_goals).page(@params[:page]).per(PER_PAGE)
       end
     end
   end
