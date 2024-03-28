72d02600a606d367ae5545f61fcf722f5d3bd323
eskokado <eskokado@gmail.com>
2024-03-28 14:10:30 -0300

    refactor(goals): impl business shared paginator into business goals fetch

--- app/businesses/goals/fetch.rb
+++ app/businesses/goals/fetch.rb
@@ -16,34 +16,9 @@ module Goals
     private
 
     def paginate_results(results)
-      paginator = Paginator.new(results, @params[:page])
-      paginator.paginate
-    end
-  end
-
-  # Extracts pagination logic into its own class
-  class Paginator
-    attr_reader :results, :page
-
-    def initialize(results, page)
-      @results = results
-      @page = page
+      paginator = ::Shared::Paginator.new(results, @params[:page], PER_PAGE)
+      paginator.call
     end
 
-    def paginate
-      return paginate_relation if results.is_a?(ActiveRecord::Relation)
-      paginate_array
-    end
-
-    private
-
-    def paginate_relation
-      results.order(created_at: :desc).page(page).per(Fetch::PER_PAGE)
-    end
-
-    def paginate_array
-      sorted_goals = results.sort_by(&:created_at).reverse
-      Kaminari.paginate_array(sorted_goals).page(page).per(Fetch::PER_PAGE)
-    end
   end
 end
