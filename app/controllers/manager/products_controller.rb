aa359808c6dddc25044024d5b3615d5a9af5cbf8
eskokado <eskokado@gmail.com>
2024-04-01 15:42:21 -0300

    refactor(product): controller

--- app/controllers/manager/products_controller.rb
+++ app/controllers/manager/products_controller.rb
@@ -1,8 +1,10 @@
 module Manager
   class ProductsController < InternalController
+    before_action :set_client, only: %i[index create]
+    before_action :build_product, only: %i[create]
+
     def index
-      client = current_user.client
-      fetch = ::Products::Fetch.new(params, client: client)
+      fetch = ::Products::Fetch.new(params, client: @client)
       @q = fetch.search
       @products = fetch.call
     end
@@ -12,24 +14,25 @@ module Manager
     end
 
     def create
-      @product = Product.new(product_params)
-      @product.client = current_user.client
-      respond_to do |format|
-        if @product.save
-          format.html do
-            redirect_to manager_product_path(@product),
-                        notice: t('controllers.manager.products.create')
-          end
-        else
-          format.html do
-            render :new,
-                   status: :unprocessable_entity
-          end
-        end
+      @product.assign_attributes(product_params.merge(client: @client))
+      if @product.save
+        redirect_to manager_product_path(@product),
+                    notice: t('controllers.manager.products.create')
+      else
+        flash.now[:alert] = t('controllers.manager.products.error')
+        render :new, status: :unprocessable_entity
       end
     end
 
     private
+
+    def set_client
+      @client = current_user.client
+    end
+
+    def build_product
+      @product = Product.new(product_params)
+    end
 
     def product_params
       params.require(:product).permit(
