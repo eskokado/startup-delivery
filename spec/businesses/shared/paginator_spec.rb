37180089f4fda18085ef6e608a931cd975965f73
eskokado <eskokado@gmail.com>
2024-04-01 10:03:04 -0300

    test(paginator): #call when results is an Array sorts and paginates the array

--- spec/businesses/shared/paginator_spec.rb
+++ spec/businesses/shared/paginator_spec.rb
@@ -19,5 +19,27 @@ RSpec.describe Shared::Paginator do
         expect(paginated_results.size).to eq(per_page)
       end
     end
+
+    context 'when results is an Array' do
+      let(:results) do
+        [
+          OpenStruct.new(created_at: 3.days.ago),
+          OpenStruct.new(created_at: 2.days.ago),
+          OpenStruct.new(created_at: 1.day.ago),
+          OpenStruct.new(created_at: Time.zone.now)
+        ]
+      end
+
+      it 'sorts and paginates the array' do
+        paginator = Shared::Paginator.new(results, page, per_page)
+        paginated_results = paginator.call
+
+        expect(paginated_results.size).to eq(2)
+        expect(paginated_results.first.created_at)
+          .to be > paginated_results.last.created_at
+        expect(paginated_results).to be_a(Kaminari::PaginatableArray)
+        expect(paginated_results).to respond_to(:total_pages)
+      end
+    end
   end
 end
