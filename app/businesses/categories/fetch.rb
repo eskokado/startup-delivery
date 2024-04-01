8fb85ad524591794870dfa2b4bddc5fcfd461816
eskokado <eskokado@gmail.com>
2024-04-01 10:47:19 -0300

    refactor(categories): impl application fetch

--- app/businesses/categories/fetch.rb
+++ app/businesses/categories/fetch.rb
@@ -1,29 +1,9 @@
 module Categories
-  class Fetch < BusinessApplication
-    PER_PAGE = 4
-    attr_reader :search
-
-    def initialize(params, client)
-      @params = params
-      @client = client
-      configure_search
-    end
-
-    def call
-      results = @search.result(distinct: true)
-      paginate_results(results)
-    end
-
+  class Fetch < ApplicationFetch
     private
 
-    def configure_search
-      search_params = @params.fetch(:q, {}).merge(client_eq: @client)
-      @search = Category.ransack(search_params)
-    end
-
-    def paginate_results(results)
-      paginator = ::Shared::Paginator.new(results, @params[:page], PER_PAGE)
-      paginator.call
+    def model_class
+      Category
     end
   end
 end
