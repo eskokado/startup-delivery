1aa785c85e316079d377630ef225fe161e4f14e6
eskokado <eskokado@gmail.com>
2024-04-01 15:18:18 -0300

    feat(product): impl method new and create

--- app/controllers/manager/products_controller.rb
+++ app/controllers/manager/products_controller.rb
@@ -6,5 +6,42 @@ module Manager
       @q = fetch.search
       @products = fetch.call
     end
+
+    def new
+      @product = Product.new
+    end
+
+    def create
+      @product = Product.new(product_params)
+      @product.client = current_user.client
+      respond_to do |format|
+        if @product.save
+          format.html do
+            redirect_to manager_product_path(@product),
+                        notice: t('controllers.manager.products.create')
+          end
+        else
+          format.html do
+            render :new,
+                   status: :unprocessable_entity
+          end
+        end
+      end
+    end
+
+    private
+
+    def product_params
+      params.require(:product).permit(
+        :name,
+        :description,
+        :long_description,
+        :photo,
+        :combo,
+        :pizza,
+        :value,
+        :category_id
+      )
+    end
   end
 end
