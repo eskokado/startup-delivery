055569b90d9a38ca34a2487addb1e1932a4e3ebc
eskokado <eskokado@gmail.com>
2024-04-02 09:27:54 -0300

    refactor(controllers): create update resource into concern ManagerActionsSupport

--- app/controllers/concerns/manager/manager_actions_support.rb
+++ app/controllers/concerns/manager/manager_actions_support.rb
@@ -2,36 +2,54 @@ module Manager
   module ManagerActionsSupport
     extend ActiveSupport::Concern
 
-    included do
-      # Define este método no seu controller para especificar o caminho de redirecionamento
-      # para o método `redirect_to_success`.
-      def path_for(resource)
-        raise NotImplementedError, 'You must implement path_for(resource)'
-      end
+    def path_for(resource)
+      raise NotImplementedError, 'You must implement path_for(resource)'
+    end
 
-      private
+    private
 
-      def redirect_to_success(resource, action)
-        redirect_to path_for(resource),
-                    notice: t("controllers.manager.#{controller_name}.#{action}")
-      end
+    def redirect_to_success(resource, action)
+      redirect_to path_for(resource),
+                  notice: t("controllers.manager.#{controller_name}.#{action}")
+    end
 
-      def render_failure(view)
-        flash.now[:alert] = t("controllers.manager.#{controller_name}.error")
-        render view, status: :unprocessable_entity
-      end
+    def render_failure(view)
+      flash.now[:alert] = t("controllers.manager.#{controller_name}.error")
+      render view, status: :unprocessable_entity
+    end
 
-      def set_current_client_context
-        @client = current_user.client
-      end
+    def set_current_client_context
+      @client = current_user.client
+    end
 
-      def create_resource(resource, success_action:, failure_view:)
-        if resource.save
-          redirect_to_success(resource, success_action)
-        else
-          render_failure(failure_view)
-        end
+    def create_resource(resource, success_action:, failure_view:)
+      if resource.save
+        redirect_to_success(resource, success_action)
+      else
+        render_failure(failure_view)
       end
+    end
+
+    def update_resource(
+      resource,
+      resource_params,
+      success_action:,
+      failure_view:,
+      purge_attachment: nil
+    )
+      purge_attachment_if_requested(resource, purge_attachment) if
+        purge_attachment
+      if resource.update(resource_params)
+        redirect_to_success(resource, success_action)
+      else
+        render_failure(failure_view)
+      end
+    end
+
+    def purge_attachment_if_requested(resource, attachment_key)
+      attachment = resource.send(attachment_key)
+      attachment.purge if
+        params[resource.model_name.param_key][attachment_key] == '1'
     end
   end
 end
