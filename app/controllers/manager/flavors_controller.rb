4713febdf2b0364bcb59c88c626c3cc6756181c0
eskokado <eskokado@gmail.com>
2024-04-04 10:39:49 -0300

    feat(flavors): impl fetch business into controller

--- app/controllers/manager/flavors_controller.rb
+++ app/controllers/manager/flavors_controller.rb
@@ -1,7 +1,12 @@
 module Manager
   class FlavorsController < InternalController
+    include ManagerActionsSupport
+
+    before_action :set_current_client_context, only: %i[index]
     def index
-      @flavors = Flavor.all
+      fetch = ::Flavors::Fetch.new(params, client: @client)
+      @q = fetch.search
+      @flavors = fetch.call
     end
   end
 end
