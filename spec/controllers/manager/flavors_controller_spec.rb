2886dd9995b30e808de5a47d0ca2b477649811cf
eskokado <eskokado@gmail.com>
2024-04-04 08:36:25 -0300

    test(flavors): controller GET #index assigns @flavors

---
+++ spec/controllers/manager/flavors_controller_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe Manager::FlavorsController, type: :controller do
+  let(:user) { FactoryBot.create(:user) }
+  let(:client) { create(:client, user: user) }
+
+  let(:flavors) { create_list(:flavor, 3, client: client) }
+  let(:flavor) { create(:flavor, client: client) }
+
+  before(:each) do
+    allow_any_instance_of(InternalController)
+      .to receive(:authenticate_user!).and_return(true)
+    allow_any_instance_of(ApplicationController)
+      .to receive(:current_user).and_return(user)
+  end
+
+  describe 'GET #index' do
+    it 'assigns @flavors' do
+      flavor = FactoryBot.create(:flavor, client: client)
+      get :index
+      expect(assigns(:flavors)).to eq([flavor])
+    end
+  end
+end
