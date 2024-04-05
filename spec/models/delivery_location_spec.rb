2bfc8982c0fa841a5bbd7e059a73db9030bc072b
eskokado <eskokado@gmail.com>
2024-04-05 07:21:02 -0300

    test(delivery_location): model is not valid without a name

--- spec/models/delivery_location_spec.rb
+++ spec/models/delivery_location_spec.rb
@@ -25,4 +25,9 @@ RSpec.describe DeliveryLocation, type: :model do
     delivery_location = FactoryBot.create(:delivery_location)
     expect(delivery_location).to be_valid
   end
+
+  it 'is not valid without a name' do
+    delivery_location = build(:delivery_location, name: nil)
+    expect(delivery_location).not_to be_valid
+  end
 end
