ea61283ec3b195d4133b5539ae0cd97a7ea7a4ee
eskokado <eskokado@gmail.com>
2024-04-05 07:15:10 -0300

    feat(delivery_location): create comment in model

--- app/models/delivery_location.rb
+++ app/models/delivery_location.rb
@@ -1,3 +1,23 @@
+# == Schema Information
+#
+# Table name: delivery_locations
+#
+#  id         :bigint           not null, primary key
+#  deleted_at :datetime
+#  name       :string
+#  value      :decimal(, )
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  client_id  :bigint           not null
+#
+# Indexes
+#
+#  index_delivery_locations_on_client_id  (client_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (client_id => clients.id)
+#
 class DeliveryLocation < ApplicationRecord
   acts_as_paranoid
 
