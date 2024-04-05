81e19636705f4c85670db66fae844de862aef019
eskokado <eskokado@gmail.com>
2024-04-05 08:05:13 -0300

    test(delivery_locations): controller method GET #index assigns @delivery_locations

---
+++ spec/controllers/manager/delivery_locations_controller_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe Manager::DeliveryLocationsController, type: :controller do
+  let(:user) { FactoryBot.create(:user) }
+  let(:client) { create(:client, user: user) }
+
+  let(:delivery_locations) { create_list(:delivery_location, 3, client: client) }
+  let(:delivery_location) { create(:delivery_location, client: client) }
+
+  before(:each) do
+    allow_any_instance_of(InternalController)
+      .to receive(:authenticate_user!).and_return(true)
+    allow_any_instance_of(ApplicationController)
+      .to receive(:current_user).and_return(user)
+  end
+
+  describe 'GET #index' do
+    it 'assigns @delivery_locations' do
+      delivery_location = FactoryBot.create(:delivery_location, client: client)
+      get :index
+      expect(assigns(:delivery_locations)).to eq([delivery_location])
+    end
+  end
+end
