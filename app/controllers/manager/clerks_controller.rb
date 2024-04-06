36e1f41f275c7624f8d3e3ef424ad65a31033757
eskokado <eskokado@gmail.com>
2024-04-06 08:57:06 -0300

    feat(clerks): impl index with fetch

--- app/controllers/manager/clerks_controller.rb
+++ app/controllers/manager/clerks_controller.rb
@@ -1,7 +1,10 @@
 module Manager
   class ClerksController < InternalController
+    include ManagerActionsSupport
+
+    before_action :set_current_client_context, only: %i[index]
     def index
-      @clerks = Clerk.all
+      index_with_fetch('Clerks')
     end
   end
 end
