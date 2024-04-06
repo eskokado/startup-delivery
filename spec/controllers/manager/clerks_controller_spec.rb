148d72a127ea802f2255688dad536451e910a254
eskokado <eskokado@gmail.com>
2024-04-06 08:33:15 -0300

    test(clerks): controller method GET #index assigns @clerks

---
+++ spec/controllers/manager/clerks_controller_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe Manager::ClerksController, type: :controller do
+  let(:user) { FactoryBot.create(:user) }
+  let(:client) { create(:client, user: user) }
+
+  let(:clerks) { create_list(:clerk, 3, client: client) }
+  let(:clerk) { create(:clerk, client: client) }
+
+  before(:each) do
+    allow_any_instance_of(InternalController)
+      .to receive(:authenticate_user!).and_return(true)
+    allow_any_instance_of(ApplicationController)
+      .to receive(:current_user).and_return(user)
+  end
+
+  describe 'GET #index' do
+    it 'assigns @clerks' do
+      clerk = FactoryBot.create(:clerk, client: client)
+      get :index
+      expect(assigns(:clerks)).to eq([clerk])
+    end
+  end
+end
