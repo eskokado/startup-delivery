1b9350d2368daad7b4c0a6345fc006ee6e8244b4
eskokado <eskokado@gmail.com>
2024-04-07 08:58:40 -0300

    feat(orders): association with order_items

--- app/models/order.rb
+++ app/models/order.rb
@@ -27,6 +27,7 @@
 #  fk_rails_...  (client_id => clients.id)
 #
 class Order < ApplicationRecord
+  acts_as_paranoid
   belongs_to :client
 
   validates :client_id, :total, :total_paid, :change, :payment_type, :date, :time, :status, presence: true
@@ -36,4 +37,14 @@ class Order < ApplicationRecord
   validates :payment_type, inclusion: { in: %w[Cash CreditCard DebitCard Transfer] }
   validates :status, inclusion: { in: %w[Pending Completed Cancelled] }
   validates :paid, inclusion: { in: %w[Yes No] }, allow_blank: true
+
+  has_many :order_items, dependent: :destroy
+
+  before_destroy :update_order_item_deleted_at, if: :persisted?
+
+  def update_order_item_deleted_at
+    order_items.find_each do |order_item|
+      order_item.update(deleted_at: Time.current)
+    end
+  end
 end
