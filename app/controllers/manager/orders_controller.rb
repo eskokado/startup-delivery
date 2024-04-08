915355a08058034ad9e5fde26e8cba0caba2208f
eskokado <eskokado@gmail.com>
2024-04-08 13:59:12 -0300

    feat(orders): add method GET show products into controller

--- app/controllers/manager/orders_controller.rb
+++ app/controllers/manager/orders_controller.rb
@@ -4,13 +4,15 @@ module Manager
 
     before_action :set_current_client_context, only: %i[index]
     before_action -> { prepare_resource(Order) },
-                  only: %i[show_consumer]
+                  only: %i[show_consumer show_products]
 
     def index
       index_with_fetch('Orders')
     end
 
     def show_consumer; end
+
+    def show_products; end
 
     private
 
