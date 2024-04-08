af2f9c93cf7d62afd4095489e500b68d910b244a
eskokado <eskokado@gmail.com>
2024-04-08 13:07:02 -0300

    fix(consumers): rubocop

--- spec/models/consumer_spec.rb
+++ spec/models/consumer_spec.rb
@@ -31,11 +31,11 @@
 require 'rails_helper'
 
 RSpec.describe Consumer, type: :model do
-  describe "validations" do
+  describe 'validations' do
     it { should validate_presence_of(:document) }
   end
 
-  describe "associations" do
+  describe 'associations' do
     it { should belong_to(:user) }
     it { should have_many(:orders).dependent(:destroy) }
   end
