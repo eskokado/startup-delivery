56c778137a64c2ca501d7769ca5dec740d7349f6
eskokado <eskokado@gmail.com>
2024-04-11 16:36:15 -0300

    test(config_delivery): controller method PATCH #update with valid attributes updates the config_delivery

--- spec/controllers/manager/config_deliveries_controller_spec.rb
+++ spec/controllers/manager/config_deliveries_controller_spec.rb
@@ -2,23 +2,23 @@ require 'rails_helper'
 
 RSpec.describe Manager::ConfigDeliveriesController,
                type: :controller do
-
   let(:user) { FactoryBot.create(:user) }
-  let(:client) { FactoryBot.create(:client, user: user) }
-  let(:config_delivery) { FactoryBot.create(:config_delivery, client: client) }
 
   before(:each) do
     allow_any_instance_of(InternalController)
       .to receive(:authenticate_user!).and_return(true)
     allow_any_instance_of(ApplicationController)
       .to receive(:current_user).and_return(user)
+
+    @client = FactoryBot.create(:client, user: user)
+    @config_delivery = FactoryBot.create(:config_delivery, client: @client)
   end
 
   describe 'GET #edit_config' do
     it 'assigns the requested config_delivery to @config_delivery' do
-      config_delivery.save!
+      @config_delivery.save!
       get :edit_config
-      expect(assigns(:config_delivery)).to eq(config_delivery)
+      expect(assigns(:config_delivery)).to eq(@config_delivery)
     end
 
     it 'renders the index template' do
@@ -29,20 +29,27 @@ RSpec.describe Manager::ConfigDeliveriesController,
 
   describe 'PATCH #update' do
     context 'with valid attributes' do
+      let(:config_delivery_attributes) {
+        FactoryBot.attributes_for(:config_delivery)
+      }
+
       it 'updates the config_delivery' do
         patch :update_config, params: {
-          config_delivery: {
-            delivery_forecast: 30,
-            delivery_fee: 6,
-            opening_time: '08:00',
-            closing_time: '23:59'
-          }
+          config_delivery: config_delivery_attributes.merge(
+            delivery_forecast: 40,
+            delivery_fee: 10,
+            opening_time: Time.zone.parse('12:00:00').strftime('%H:%M'),
+            closing_time: Time.zone.parse('22:59:00').strftime('%H:%M')
+          )
         }
-        config_delivery.reload
-        expect(config_delivery.delivery_forecast).to eq(30)
-        expect(config_delivery.delivery_fee).to eq(6)
-        expect(config_delivery.opening_time.strftime('%H:%M')).to eq('08:00')
-        expect(config_delivery.closing_time.strftime('%H:%M')).to eq('23:59')
+        @config_delivery.reload
+        expect(@config_delivery.delivery_forecast).to eq(40)
+        expect(@config_delivery.delivery_fee).to eq(10)
+        expect(@config_delivery.opening_time.strftime('%H:%M')).to eq('12:00')
+        expect(@config_delivery.closing_time.strftime('%H:%M')).to eq('22:59')
+        expect(response).to redirect_to(manager_edit_config_path)
+        expect(flash[:notice])
+          .to eq I18n.t('controllers.manager.config_deliveries.update')
       end
     end
   end
