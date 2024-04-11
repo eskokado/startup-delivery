891d9866805b5965f9657ee0517f27501c4fa792
eskokado <eskokado@gmail.com>
2024-04-11 13:36:30 -0300

    test(config_delivery): controller method GET #edit_config assigns the requested config_delivery to @config_delivery

---
+++ spec/controllers/manager/config_deliveries_controller_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe Manager::ConfigDeliveriesController,
+               type: :controller do
+
+  let(:user) { FactoryBot.create(:user) }
+  let(:client) { FactoryBot.create(:client, user: user) }
+  let(:config_delivery) { FactoryBot.create(:config_delivery, client: client) }
+
+  before(:each) do
+    allow_any_instance_of(InternalController)
+      .to receive(:authenticate_user!).and_return(true)
+    allow_any_instance_of(ApplicationController)
+      .to receive(:current_user).and_return(user)
+  end
+
+  describe 'GET #edit_config' do
+    it 'assigns the requested config_delivery to @config_delivery' do
+      config_delivery.save!
+      get :edit_config
+      expect(assigns(:config_delivery)).to eq(config_delivery)
+    end
+  end
+end
