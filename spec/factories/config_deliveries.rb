f06758d55b3493faeb638e33d1bc105891f18b18
eskokado <eskokado@gmail.com>
2024-04-11 13:00:05 -0300

    feat(config_delivery): create factory

---
+++ spec/factories/config_deliveries.rb
@@ -0,0 +1,32 @@
+# == Schema Information
+#
+# Table name: config_deliveries
+#
+#  id                :bigint           not null, primary key
+#  closing_time      :time
+#  deleted_at        :datetime
+#  delivery_fee      :decimal(, )
+#  delivery_forecast :integer
+#  opening_time      :time
+#  created_at        :datetime         not null
+#  updated_at        :datetime         not null
+#  client_id         :bigint           not null
+#
+# Indexes
+#
+#  index_config_deliveries_on_client_id  (client_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (client_id => clients.id)
+#
+FactoryBot.define do
+  factory :config_delivery do
+    delivery_forecast { 30 }
+    delivery_fee { 6 }
+    opening_time { '08:00' }
+    closing_time { '23:59' }
+    association :client, factory: :client
+    deleted_at { nil }
+  end
+end
