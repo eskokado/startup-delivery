d77b31ad59e97ed4eb22c8625bbd9e73e7ca1771
eskokado <eskokado@gmail.com>
2024-04-11 13:01:24 -0300

    feat(config_delivery): create controller

---
+++ app/controllers/manager/config_deliveries_controller.rb
@@ -0,0 +1,49 @@
+module Manager
+  class ConfigDeliveriesController < InternalController
+    before_action :set_client, only: %i[edit_config update_config]
+    before_action :set_config_delivery, only: %i[edit_config update_config]
+
+    def edit_config; end
+
+    def update_config
+      if @config_delivery.id.nil?
+        create_config_delivery
+      else
+        update_config_delivery
+      end
+
+      redirect_to manager_edit_config_path,
+                  notice: t('controllers.manager.config_deliveries.update')
+    rescue StandardError
+      redirect_to manager_edit_config_path,
+                  notice: t('controllers.manager.config_deliveries.error')
+    end
+
+    private
+
+    def config_delivery_params
+      params.require(:config_delivery).permit(
+        :delivery_forecast, :delivery_fee, :opening_time, :closing_time
+      )
+    end
+
+    def set_client
+      @client = current_user.client
+    end
+
+    def create_config_delivery
+      @config_delivery = ConfigDelivery.new(config_delivery_params)
+      @config_delivery.client = current_user.client
+      @config_delivery.save!
+    end
+
+    def update_config_delivery
+      @config_delivery.update!(config_delivery_params)
+    end
+
+    def set_config_delivery
+      @config_delivery = ConfigDelivery
+                         .find_by(client: @client) || ConfigDelivery.new
+    end
+  end
+end
