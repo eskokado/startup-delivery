07092e542170238f361f05902632de23b8afa994
eskokado <eskokado@gmail.com>
2024-04-15 14:28:14 -0300

    test(home): fix controller method GET #index assigns @total_clients, @total_orders_day, @total_orders_waiting, @total_sold

--- spec/controllers/manager/home_controller_spec.rb
+++ spec/controllers/manager/home_controller_spec.rb
@@ -1,34 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe Manager::HomeController, type: :controller do
-  describe "GET #index" do
-    before do
-      # Setup test data
-      create_list(:client, 10)
-      create_list(:order, 5, date: Time.zone.today, status: 'Completed')
-      create_list(:order, 3, date: Time.zone.today, status: 'Waiting')
-      create(:order, date: 1.day.ago, status: 'Completed') # Not to be counted for today
-      create(:order, date: Time.zone.today, status: 'Completed', total: 100)
-    end
+  let(:user) { FactoryBot.create(:user) }
 
-    it "assigns @total_clients" do
+  before(:each) do
+    allow_any_instance_of(InternalController)
+      .to receive(:authenticate_user!).and_return(true)
+    allow_any_instance_of(ApplicationController)
+      .to receive(:current_user).and_return(user)
+  end
+
+  describe 'GET #index' do
+    it 'assigns @total_clients' do
+      create_list(:client, 10)
       get :index
       expect(assigns(:total_clients)).to eq(10)
     end
 
-    it "assigns @total_orders_day" do
+    it 'assigns @total_orders_day' do
+      create_list(:order, 5, date: Time.zone.today, status: 'Completed')
+      create_list(:order, 3, date: Time.zone.today, status: 'Waiting')
+      create(:order, date: 1.day.ago, status: 'Completed')
       get :index
-      expect(assigns(:total_orders_day)).to eq(8) # 5 Completed + 3 Waiting
+      expect(assigns(:total_orders_day)).to eq(8)
     end
 
-    it "assigns @total_orders_waiting" do
+    it 'assigns @total_orders_waiting' do
+      create_list(:order, 3, date: Time.zone.today, status: 'Waiting')
       get :index
       expect(assigns(:total_orders_waiting)).to eq(3)
     end
 
-    it "assigns @total_sold" do
+    it 'assigns @total_sold' do
+      create(:order, date: Time.zone.today, status: 'Completed', total: 100)
       get :index
-      expect(assigns(:total_sold)).to eq(500) # Assuming each order has a default total of 100
+      expect(assigns(:total_sold)).to eq(100)
     end
   end
 end
