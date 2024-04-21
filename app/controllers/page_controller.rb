ee121e93bb68007497a0a00b897d1b1b3a4ff3c8
eskokado <eskokado@gmail.com>
2024-04-21 07:49:49 -0300

    fix: rubocop

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -1,7 +1,7 @@
 class PageController < ApplicationController
-  LAYOUT_ACTIONS = %i[integrations team billing notifications settings activity profile
-                     people calendar assignments message messages project projects
-                     dashboard].freeze
+  LAYOUT_ACTIONS = %i[integrations team billing notifications settings
+                      activity profile people calendar assignments
+                      message messages project projects dashboard].freeze
 
   LAYOUT_ACTIONS.each do |action|
     define_method(action) do
