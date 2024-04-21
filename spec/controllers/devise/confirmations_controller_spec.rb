f85e6c3b9712d7d910885415cc39f2c10806681d
eskokado <eskokado@gmail.com>
2024-04-21 12:39:10 -0300

    fix: rubocop

--- spec/controllers/devise/confirmations_controller_spec.rb
+++ spec/controllers/devise/confirmations_controller_spec.rb
@@ -1,6 +1,6 @@
 RSpec.describe Devise::ConfirmationsController, type: :controller do
   before do
-    @request.env["devise.mapping"] = Devise.mappings[:user]
+    @request.env['devise.mapping'] = Devise.mappings[:user]
   end
 
   describe 'GET #show' do
