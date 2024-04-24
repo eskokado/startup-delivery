2c2244c1fe240ec255c88640005001a95d9f61ef
eskokado <eskokado@gmail.com>
2024-04-24 12:16:02 -0300

    feat(posts): migration and schema

---
+++ db/migrate/20240423112433_create_posts.rb
@@ -0,0 +1,14 @@
+class CreatePosts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :posts do |t|
+      t.string :title
+      t.text :content
+      t.string :image_url
+      t.references :client, null: false, foreign_key: true
+      t.datetime :deleted_at
+
+      t.timestamps
+    end
+    add_index :posts, :deleted_at
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_04_20_130903) do
+ActiveRecord::Schema[7.0].define(version: 2024_04_23_112433) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -221,6 +221,18 @@ ActiveRecord::Schema[7.0].define(version: 2024_04_20_130903) do
     t.index ["consumer_id"], name: "index_orders_on_consumer_id"
   end
 
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.string "image_url"
+    t.bigint "client_id", null: false
+    t.datetime "deleted_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["client_id"], name: "index_posts_on_client_id"
+    t.index ["deleted_at"], name: "index_posts_on_deleted_at"
+  end
+
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.string "description"
@@ -296,6 +308,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_04_20_130903) do
   add_foreign_key "order_items", "products"
   add_foreign_key "orders", "clients"
   add_foreign_key "orders", "consumers"
+  add_foreign_key "posts", "clients"
   add_foreign_key "products", "categories"
   add_foreign_key "products", "clients"
 end
